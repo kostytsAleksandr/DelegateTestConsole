9c3dff7b1755e3893a83e4b6a86fb3a53c59df59
kostytsAleksandr <kostyts.aleksandr@gmail.com>
2020-12-21 20:09:47 +0200

    emptyProj

---
+++ DelegateTestConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DelegateTestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
